d711c9775a00f5df17d57dee64b11ac35919b304
James Laptop <mjolnir365@gmail.com>
2021-11-22 08:27:48 -0700

    initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,81 @@
+import colors from 'vuetify/es5/util/colors'
+
+export default {
+  image: {
+        // The screen sizes predefined by `@nuxt/image`:
+        screens: {
+          xs: 320,
+          sm: 640,
+          md: 768,
+          lg: 1024,
+          xl: 1280,
+          xxl: 1536,
+          '2xl': 1536
+        },
+    },
+  target: 'static',
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    titleTemplate: '%s - cpnt200-a1',
+    title: 'cpnt200-a1',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/vuetify
+    '@nuxtjs/vuetify',
+    '@nuxt/image'
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    '@nuxt/content',
+    '@nuxt/image'
+  ],
+
+  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: true,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
+    }
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
---
+++ pages/jokes/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>Jokes go here</h1>
+    <p>If I knew any...</p>
+    <br>
+    <div class="gallery">
+      <nuxt-img src="/chuckle.jpeg" sizes="sm:100px md:200px lg:400px" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "Dad 'Jokes'",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best place for corny jokes",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+
+.gallery {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  min-width: 400px;
+}
+</style>
